858bbb454326f1c9ba5f260b244b25f02d053800
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2022-10-02 02:35:55 +0700

    Add project files.

---
+++ SalesWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace SalesWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public bool isAdmin = false;
+        public MainWindow(bool isAdmin = false)
+        {
+            InitializeComponent();
+            this.isAdmin = isAdmin;
+            this.DataContext = this;
+        }
+    }
+}
